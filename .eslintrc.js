97a23410b064e1564bc94dc0ffb9afe9cec0a9a9
Adam Laptop <afoskit@islander.tamucc.edu>
2020-01-18 10:37:26 -0600

    updating boiler

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,12 @@
-module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true
+module.exports =  {
+    parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
+    extends:  [
+      'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+      'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+      'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    ],
+    parserOptions:  {
+      ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
+      sourceType:  'module',  // Allows for the use of imports
     },
-    "parser": '@typescript-eslint/parser',
-    "extends": ["eslint:recommended", 'plugin:@typescript-eslint/recommended', "plugin:prettier/recommended"],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018
-    },
-    "rules": {
-        "camelcase": "off",
-        "@typescript-eslint/camelcase": ["error", { "properties": "never" }],
-        "comma-dangle": ["error", {
-            "arrays": "never",
-            "objects": "never",
-            "imports": "never",
-            "exports": "never",
-            "functions": "never"
-        }],
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "prettier/prettier": "error"
-    }
-};
+  };
